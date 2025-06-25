d859f25f4007acc5df18cacc1e84901d8ed5fadd
Kabir <adkabir@julius-berger.com>
2025-06-25 10:04:49 +0100

    This is my first  branch  commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+x = [f"10.11.3.{i}" for i in range(0, 10)]
+
+print(x)
+
+y = "this is my second commit"
+
+z = "this is my third commit"
+
+za = "this is my fourth commit"
+
+a = "This is a branch modification"
+b = "This is a second branch modification"
+
+
+
+
+
