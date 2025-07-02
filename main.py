2fea207a04cfe8c9371270648f1952291174147b
ak47magnum <102174072+ak47magnum@users.noreply.github.com>
2025-07-02 09:55:33 +0000

    Deleted some code

--- main.py
+++ main.py
@@ -3,11 +3,3 @@
 x = [f"10.11.3.{i}" for i in range(0, 10)]
 
 print(x)
-
-y = "this is my second commit"
-
-z = "this is my third commit"
-
-za = "this is my fourth commit"
-
-final_step = "This will be the true sense of what github is. tThis will be the final merge"
