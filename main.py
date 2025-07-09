9f27ec5ae7d489dc99ec388e37c88db7a5a6c0e6
alameenpjs <alameenpjs@gmail.com>
2025-07-09 14:17:32 +0100

    Update main.py
    function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
+from data import adding
 
 x = [f"10.11.3.{i}" for i in range(0, 10)]
 
 print(x)
 
 print("THIS IS MY ADDITION TO THE CODE BASE") # additional code by alameenpjs
+
+c = adding(3,4)
+print(c)
